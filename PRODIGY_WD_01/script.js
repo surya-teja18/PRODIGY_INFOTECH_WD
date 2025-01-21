b894f88f75975d9d48ad20e2aa077171c39997e0
BEJAWADA SURYA TEJA <suryabejawada900@gmail.com>
2025-01-21 21:15:26 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,11 @@
+// Select the navbar
+const navbar = document.querySelector('.navbar');
+
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
